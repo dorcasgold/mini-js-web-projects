3f4f3d6f77055f61a2b9edb58e15a83984037107
odetayo dorcas <odetayodorcas123@gmail.com>
2024-01-12 17:56:33 +0100

    Add new folder

---
+++ js-cards/scripts/products.js
@@ -0,0 +1,38 @@
+export const products = [
+  {
+    image: 'luxury-tower-set-6-piece.jpg',
+    name: 'luxury tower set 6 piece',
+    rating: {
+      stars: 5,
+      count: 95
+    },
+    priceCents: 1090,
+  },
+  {
+    image: 'luxury-tower-set-6-piece.jpg',
+    name: 'luxury tower set 6 piece',
+    rating: {
+      stars: 5,
+      count: 95
+    },
+    priceCents: 1090,
+  },
+  {
+    image: 'luxury-tower-set-6-piece.jpg',
+    name: 'luxury tower set 6 piece',
+    rating: {
+      stars: 5,
+      count: 95
+    },
+    priceCents: 1090,
+  },
+  {
+    image: 'luxury-tower-set-6-piece.jpg',
+    name: 'luxury tower set 6 piece',
+    rating: {
+      stars: 5,
+      count: 95
+    },
+    priceCents: 1090,
+  }
+]
